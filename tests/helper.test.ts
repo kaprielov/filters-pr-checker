a137716935a9ac17cd04ddb31c7fe25eb00b0ecf
Anton <a.kaprielov@adguard.com>
2022-06-02 14:24:23 +0300

    test-13

--- tests/helper.test.ts
+++ tests/helper.test.ts
@@ -1,11 +1,12 @@
-import { getUrlFromDescription } from '../src/helpers';
+import { URL_MARK } from '../src/constants';
+import { getValueFromDescription } from '../src/helpers';
 
 describe('helpers', () => {
     describe('getUrlFromDescription', () => {
         it('parses url', () => {
             const url = 'https://example.org';
             const text = `#url: ${url}`;
-            expect(getUrlFromDescription(text)).toBe(url);
+            expect(getValueFromDescription(text, URL_MARK)).toBe(url);
         });
 
         it('parses url in the multiple string description', () => {
@@ -14,7 +15,7 @@ describe('helpers', () => {
             this is my pr
             #url: ${url}
             `;
-            expect(getUrlFromDescription(text)).toBe(url);
+            expect(getValueFromDescription(text, URL_MARK)).toBe(url);
         });
     });
 });
